d294a6c53d8ce66e2d1a809dbb0632f2b34808e5
ArcLKN <kalidolkn@gmail.com>
2025-01-25 03:10:20 +0100

    Added english language entries

--- src/pages/AccueilPage/accueil2.jsx
+++ src/pages/AccueilPage/accueil2.jsx
@@ -5,8 +5,11 @@ export default function Accueil2 ({ language }) {
     return (
         <Center bg="white" w="100%" h="100vh" m={0} overflow="hidden" p={20}>
             <VStack direction={"column"} spacing={16} textAlign="center" p={20}>
-                <Text fontSize={25}>Faire campus et innover</Text>
-                <Text fontSize={60} fontWeight={"semibold"} color={"lightgray"} lineHeight={1.2}>Dans nos campus, nous transformons les innovations deeptech en solutions industrielles à impact.</Text>
+                <Text fontSize={25}>{language === "FR" ? "Faire campus et innover" : "Campus and innovation"}</Text>
+                <Text fontSize={60} fontWeight={"semibold"} color={"lightgray"} lineHeight={1.2}>
+                {language === "FR" ? "Dans nos campus, nous transformons les innovations deeptech en solutions industrielles à impact."
+                : "On our campuses, we transform deeptech innovations into high-impact industrial solutions."}
+                </Text>
                 <HStack align={"center"} spacing={10}>
                     <Image src='../../../public/Certified_B_Corporation_B_Corp_Logo_2022_Black_RGB.svg.png' objectFit='contain' h="200px" w="auto"/>
                     <Image src='../../../public/logo-TI29Grey.png' objectFit='contain' w="300px" h="auto"/>
