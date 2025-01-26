97fcd5c8a4dee4ef4ec74fb31ba07965ca3e88c5
ArcLKN <kalidolkn@gmail.com>
2025-01-26 16:37:19 +0100

    Added english language

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,24 +7,29 @@ export default function Footer({ language }) {
         <Flex direction={"column"} align={"center"}>
             <Flex w="full" align={"center"} justify={"space-between"}>
                 <HStack>
-                    <Image src="/public/Certified_B_Corporation_B_Corp_Logo_2022_Black_RGB.svg.png" boxSize={28} objectFit='contain'/>
-                    <Image src="/public/Logo_France_2030.svg" boxSize={24} objectFit='contain'/>
+                    <Image src="/Certified_B_Corporation_B_Corp_Logo_2022_White.png" boxSize={28} objectFit='contain'/>
+                    <Image src="/Logo_France_2030_White.png" boxSize={24} objectFit='contain'/>
                 </HStack>
                 <Text opacity={0.6} fontWeight={"hairline"}>
                     &gt; IXCONNECT
                 </Text>
             </Flex>
             <HStack spacing={10} align={"center"} w={"100%"} justifyContent={"center"} fontSize={25} mt={12}>
-                <Text>LE CAMPUS</Text><Text>L'INCUBATEUR</Text><Text>NOTRE SAVOIR FAIRE</Text><Text>NOUS CONNAÎTRE</Text>
+                <Text>{language === "FR" ? "LE CAMPUS" : "THE CAMPUS"}</Text>
+                <Text>{language === "FR" ? "L'INCUBATEUR" : "THE INCUBATOR"}</Text>
+                <Text>{language === "FR" ? "NOTRE SAVOIR FAIRE" : "OUR KNOW-HOW"}</Text>
+                <Text>{language === "FR" ? "NOUS CONNAÎTRE" : "ABOUT US"}</Text>
             </HStack>
             <Flex w="full" justifyContent={"flex-end"} mt={12}>
-                <Text opacity={0.6} fontWeight={"hairline"}>Nous contacter</Text>
+                <Text opacity={0.6} fontWeight={"hairline"}>{language === "FR" ? "Nous contacter" : "Contact us"}</Text>
             </Flex>
             <Text fontWeight={"bold"} fontSize={40}>
             I X C A M P U S
             </Text>
             <HStack spacing={8} align={"center"} w={"100%"} justifyContent={"center"} fontSize={10} mt={6}>
-                <Text>Mentions légales</Text><Text>Politique de confidentialité</Text><Text>Gestion des données personnelles</Text>
+                <Text>{language === "FR" ? "Mentions légales" : "Legal Notice"}</Text>
+                <Text>{language === "FR" ? "Politique de confidentialité" : "Privacy Policy"}</Text>
+                <Text>{language === "FR" ? "Gestion des données personnelles" : "Personal Data Management"}</Text>
             </HStack>
         </Flex>
     </Center>
